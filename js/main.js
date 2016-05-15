11cd14c4d29bfb3c1f06f133c88507fc088b565c
Kyle Firstenberg <kylefberg@me.com>
2016-05-15 10:47:54 -0700

    Both start and reset buttons are clickable.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@
 
 
 
-
+// My array of jawas.
 var sandCrawler = [
   "jawa0", "jawa1", "jawa2", "jawa3", "jawa4",
   "jawa5", "jawa6", "jawa7", "jawa8", "jawa9",];
 
 
+
+// Event Listeners
+startButton.addEventListener("click", startGame);
+
+restartButton.addEventListener("click", startGame);
 
 
