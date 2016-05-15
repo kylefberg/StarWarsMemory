6c18322795f92b58833a09986917c7fcb3170f02
Kyle Firstenberg <kylefberg@me.com>
2016-05-15 09:11:26 -0700

    added a reset button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 
 var sandCrawler = [
-  "jawa0", "jawa1", "jawa2", "jawa3",
-  "jawa4", "jawa5", "jawa6", "jawa7",
-  "jawa8", "jawa9", "jawa10", "jawa11"];
+  "jawa0", "jawa1", "jawa2", "jawa3", "jawa4",
+  "jawa5", "jawa6", "jawa7", "jawa8", "jawa9",];
 
 
 
