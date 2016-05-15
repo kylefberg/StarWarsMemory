6a8285b0fd441fa8f00f6a94dc071f2d24b2aa63
Kyle Firstenberg <kylefberg@me.com>
2016-05-15 12:43:58 -0700

    added a timer countdown from 45 but need to build a box that it sits in and count down above and below it.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 
+var startGame = function(){
+  // start clock
+  // make cards clickable
+
+}
+
+var resetGame = function(){
+  // reset clock
+  // flip cards over
+  // shuffle cards
+}
+
+
+  // Setting up the timer with a countdown of 30 seconds
+
+
+function timer() {
+  count=count -1;
+  if (count <= 0) {
+    clearInterval(counter, endGame);
+    return;
+  }
+  document.getElementById("timer").innerHTML=count + "secs";
+}
+
+var count=45;
+var counter=setInterval(timer, 1000);
 
 
 
@@ -12,6 +39,20 @@ var sandCrawler = [
 // Event Listeners
 startButton.addEventListener("click", startGame);
 
-restartButton.addEventListener("click", startGame);
+resetButton.addEventListener("click", resetGame);
+
+// var jawa = document.querySelectorAll("jawa");
+// jawa[0].addEventListener("click");
+// jawa[1].addEventListener("click");
+// jawa[2].addEventListener("click");
+// jawa[3].addEventListener("click");
+// jawa[4].addEventListener("click");
+// jawa[5].addEventListener("click");
+// jawa[6].addEventListener("click");
+// jawa[7].addEventListener("click");
+// jawa[8].addEventListener("click");
+// jawa[9].addEventListener("click");
+
+
 
 
