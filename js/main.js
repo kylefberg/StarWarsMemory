0a15ef983a025f4181ca929573d9d1678fe0ddf9
Kyle Firstenberg <kylefberg@me.com>
2016-05-16 11:54:40 -0700

    cleaned up the timer code and am working on card function

--- js/main.js
+++ js/main.js
@@ -13,33 +13,70 @@ var resetGame = function(){
 
 
   // Setting up the timer with a countdown of 30 seconds
+  // Trying to write code that makes the countdown start on
+  // the start click.
+// $(function() {
+  // });
+var count=31;
+
 function timer() {
-  count=count -1;
-  if (count <= -1) {
-    clearTimeout(timer, endGame);
+  count -= 1;
+  $("#timer").text(count + " sec");
+  if (count <= 0) {
+    // clearTimeout(timer, endGame);
     return;
+  } else {
+    setTimeout(timer, 1000);
   }
-  document.getElementById("timer").innerHTML=count + " sec";
 }
 
-var count=31;
-var counter=setInterval(timer, 1000);
 
-
+// var countdown = {
+//   count: 30,
+//   timerId: null,
+//   start: function() {
+//     this.timerId = setInterval(()) => {
+//       this.count--;
+//       console.log(this.count);
+//     }, 1000);
+//   },
+//   stop: function() {
+//     console.log(this);
+//     clearInterval(this.timerId);
+//   }
+// };
 
 // My array of jawas.
 var sandCrawler = [
-  "jawa0", "jawa1", "jawa2", "jawa3", "jawa4",
-  "jawa5", "jawa6", "jawa7", "jawa8", "jawa9",];
+  "jawa0", "jawa1", "jawa2", "jawa3", "jawa4", "jawa5", "jawa6"];
 
 
+var board = [null, null, null, null, null, null, null, null, null, null, null, null]; {
+  fipped: false;
+  className: "jawa0";
+}
 
 // Event Listeners
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", timer);
+
+
 
 resetButton.addEventListener("click", resetGame);
 
-// var jawa = document.querySelectorAll("jawa");
+
+
+// I will need to write a function connected to the click that holds the
+// cards and compaires a match.
+
+// $(.sandCrawler).on("click", function()) {
+// $(this).toggleClass('flip');
+// }
+
+
+
+
+
+// var sandCrawler = document.querySelectorAll("jawa");
 // jawa[0].addEventListener("click");
 // jawa[1].addEventListener("click");
 // jawa[2].addEventListener("click");
