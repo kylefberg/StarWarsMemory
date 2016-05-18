4478412d81fba79dbff7489c29de9b4a7646a50f
Kyle Firstenberg <kylefberg@me.com>
2016-05-18 08:27:30 -0700

    new day push

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// My array of jawas.
+// My array of cells.
 var classArray = [
   "cell0", "cell1", "cell2", "cell3", "cell4", "cell5",
   "cell0", "cell1", "cell2", "cell3", "cell4", "cell5"
@@ -18,10 +18,9 @@ function makeBoard() {
   for (var i = 0; i < 12; i++) {
     board.push(shuffled.pop());
   }
-}
+};
 
-  // start clock
-  // make cards clickable
+  // Start game, reset board, start timer
 var startGame = function() {
   gameWon     = false;
   makeBoard();
@@ -32,7 +31,7 @@ var winGame = function() {
   var opencell  = [""];
   var closedcell= [0, 1, 2, 3, 4, 5,
                6, 7, 8, 9, 10, 11];
-}
+};
 
   // reset clock
   // flip cards over
@@ -44,38 +43,40 @@ var resetGame = function(){
 };
 
 
-// BEHAVIOR
-// $(this).toggleClass('flip');
+// Choosing two cards.
+// for (var i = 0; i <=11; i++) {
+  function setChoice(index) {
+    if (choice1 === undefined) {
+      choice1 = index;
+    }  else if (choice2 === undefined) {
+      choice2 = index;
+      matched(choice1, choice2);
+    }
+  };
+// }
 
+
+// Rules for match
 var matched = function(var1, var2) {
   if (board[var1] === board[var2]) {
+    //cards stay in place, no response after first round ends
     console.log("YAAAA!")
+
   } else {
     console.log("crap...");
-  }
-}
+    //starts choice function over returns cards to face down
 
-function setChoice(index) {
-  if (choice1 === undefined) {
-    choice1 = index;
-  } else if (choice2 === undefined) {
-    choice2 = index;
-    matched(choice1, choice2);
   }
-}
+};
 
+
+//Event listener for card click and flip
 $('.board').on("click", ".card", function() {
   var flipped = $(this).toggleClass("flipped");
   console.log(board[this.id.substring(4)]);
   $(this).removeClass("back-red").addClass(board[this.id.substring(4)]);
 
-
-// Var rules on if two selected cards match + no other cards can be selected
-  //Possibly work on a for loop to deturmine if it is a match.
-
-
-
-});
+ });
 
 
 
