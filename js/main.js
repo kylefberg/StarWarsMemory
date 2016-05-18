276c869c47251de918f82de18753fecbf1192763
Kyle Firstenberg <kylefberg@me.com>
2016-05-18 11:10:56 -0700

    clean up code pass

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 // My array of cells.
 var classArray = [
-  "cell0", "cell1", "cell2", "cell3", "cell4", "cell5",
-  "cell0", "cell1", "cell2", "cell3", "cell4", "cell5"
+  "leah", "chewie", "hans", "storm1", "luke", "storm2",
+  "leah", "chewie", "hans", "storm1", "luke", "storm2"
 ];
-
 
 // My board
 var board = [];
@@ -12,55 +11,62 @@ var choice2;
 var matched;
 var gameWon = null;
 
-function makeBoard() {
+var renderState = function() {
+  console.log(
+    "Choice 1:", choice1,
+    "Choice 2:", choice2,
+    `(${matched})`
+  );
+};
+
+var makeBoard = function() {
   var shuffled = _.shuffle(classArray);
   for (var i = 0; i < 12; i++) {
     board.push(shuffled.pop());
   }
 };
 
-  // Start game, reset board, start timer
+// Start game, reset board, start timer
 var startGame = function() {
-  gameWon     = false;
+  gameWon = false;
   matched = false;
   makeBoard();
   timer();
 };
 
 var winGame = function() {
-  var opencell  = [""];
+  var opencell  = [];
   var closedcell= [0, 1, 2, 3, 4, 5,
-               6, 7, 8, 9, 10, 11];
+                   6, 7, 8, 9, 10, 11];
 };
 
-  // reset clock
-  // flip cards over
-  // shuffle cards
+// reset clock
+// flip cards over
+// shuffle cards
 var resetGame = function(){
   board = [];
   makeBoard();
-
 };
 
 // Choosing two cards.
 // I need this function to repeat clearing choice 1 and 2
 // when no match is found
-function setChoice(index) {
+var setChoice = function(index) {
   if (choice1 === undefined) {
-      choice1 = index;
+    choice1 = index;
   } else if (choice2 === undefined) {
     choice2 = index;
     matched = match(choice1, choice2);
-  } else if (choice1 !== matched && choice2 !== matched){
-    $(this).removeClass(board[this.id.substring(4)]).addClass("back-red");
-    } else {
-      (choice1 && choice2 !== undefined);
+    // After the second choice it will not take in any more choices
+  } else if (choice1 !== matched && choice2 !== matched) {
+    // No match return cards face down
+    // ({
+
+    // }, 500);
+  } else {
+    // Start new turn
   }
 }
-
-// for (var i = 0; i <=11; i++) {
-// }
-
 
 // Rules for match
 var match = function(choice1, choice2) {
@@ -70,28 +76,13 @@ var match = function(choice1, choice2) {
   } else {
     console.log("crap...");
     setTimeout(function() {
-
-    // I need a function that when a match is not found returns card face down.
-    //cards stay in place, no response after first round ends
-    }, 500);
-    return false;
-  }
+    }
+  )}
 };
 
-
-//Event listener for card click and flip
-$('.board').on("click", ".card", function() {
-  var flipped = $(this).toggleClass("flipped");
-  console.log(board[this.id.substring(4)]);
-  $(this).removeClass("back-red").addClass(board[this.id.substring(4)]);
-
- });
-
-
-
-  // Setting up the timer with a countdown of 30 seconds
-    // clearTimeout(timer, endGame);
-var count=31;
+// Setting up the timer with a countdown of 30 seconds
+// clearTimeout(timer, endGame);
+var count = 31;
 
 var timer = function() {
   count -= 1;
@@ -103,19 +94,15 @@ var timer = function() {
   }
 };
 
-
-
 // Event Listeners
+
+// Start/reset game
 $(startButton).on("click", startGame);
 $(resetButton).on("click", resetGame);
 
-
-
-
-
-
-
-
-
-
-
+// Card click and flip
+$('.board').on("click", ".card", function() {
+  var flipped = $(this).toggleClass("flipped");
+  console.log(board[this.id.substring(4)]);
+  $(this).removeClass("back-red").addClass(board[this.id.substring(4)]);
+});
